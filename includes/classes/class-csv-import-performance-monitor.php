0fd2d208ab69f97304eff58bbacb0550c79e4b1c
Michael <designare.design@gmail.com>
2025-10-01 08:33:57 +0200

    Update class-csv-import-performance-monitor.php

--- includes/classes/class-csv-import-performance-monitor.php
+++ includes/classes/class-csv-import-performance-monitor.php
@@ -8,17 +8,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 // ===================================================================
 
 class CSV_Import_Performance_Monitor {
-
+    
     private static $start_time;
     private static $start_memory;
 
     public static function start() {
         self::$start_time = microtime(true);
         self::$start_memory = memory_get_usage();
-
+        
         add_action('shutdown', [__CLASS__, 'log_performance']);
     }
-
+    
     public static function log_performance() {
         if (!self::$start_time) {
             return;
@@ -26,7 +26,7 @@ class CSV_Import_Performance_Monitor {
 
         $end_time = microtime(true);
         $end_memory = memory_get_usage();
-
+        
         $execution_time = $end_time - self::$start_time;
         $memory_used = $end_memory - self::$start_memory;
 
